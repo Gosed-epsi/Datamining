ba701ac80d25afffa54abc5fba99de2fdcb5e79c
Gosed-epsi <gosselet.edgar@gmail.com>
2015-12-07 14:07:35 +0100

    deplacement du traitement dans la classe adequat

--- Datamining/src/epsi/i5/datamining/Datamining.java
+++ Datamining/src/epsi/i5/datamining/Datamining.java
@@ -6,11 +6,6 @@
 package epsi.i5.datamining;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.List;
-import java.util.Map.Entry;
 import org.json.simple.parser.ParseException;
 
 /**
@@ -19,13 +14,6 @@ import org.json.simple.parser.ParseException;
  */
 public class Datamining {
 
-    StopWords stopWords = new StopWords();
-
-    private static Pattern pattern;
-    private static Matcher matcher;
-    private static List<DataEntity> lCommentaires;
-    private static HashMap<String, HashMap<String, Integer>> mapCategorie = new HashMap();
-
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
@@ -33,56 +21,8 @@ public class Datamining {
      */
     public static void main(String[] args) throws IOException, ParseException {
 
-        JsonBuilder builder = new JsonBuilder();
-        StopWords stopword = new StopWords();
-        boolean bStopWord = false;
-        lCommentaires = builder.listeCommentaires;
-        for (DataEntity entity : builder.listeCommentaires) {
-            String lTrie = "";
+        Traitement traitement = new Traitement();
+        traitement.traitement();
 
-            for (String cat : entity.getCategorie()) {
-                if (!mapCategorie.containsKey(cat)) {
-                    mapCategorie.put(cat, new HashMap<String, Integer>());
-                }
-            }
-
-            for (String word : entity.getCommentaires().split(" ")) {
-                //System.out.println(stopword.getRegEx());
-                for (String stopApo : stopword.getRegExApos().replace("|", " ").split(" ")) {
-                    word = word.replaceAll(stopApo, "");
-//                    System.out.println(stopApo);
-                }
-
-                word = word.replace(".", " ").replace(",", " ").replace("!", " ").replace("(", "").replace(")", "").replace("'", "").replace(":", "").trim();
-
-                for (String stop : stopword.getRegEx().replace("|", " ").split(" ")) {
-                    //System.out.println(stop);
-                    if ((word.equalsIgnoreCase(stop)) && !word.equalsIgnoreCase("")) {
-                        bStopWord = true;
-                    }
-                }
-                if (bStopWord == false && !"".equals(word)) {
-                    lTrie = lTrie + " " + word;
-                    for (String cat : entity.getCategorie()) {
-                        if (mapCategorie.get(cat).containsKey(word)) {
-                            Integer occurs = mapCategorie.get(cat).get(word);
-                            occurs++;
-                            mapCategorie.get(cat).remove(word);
-                            mapCategorie.get(cat).put(word, occurs);
-                        } else {
-                            mapCategorie.get(cat).put(word, 1);
-                        }
-                    }
-                }
-                bStopWord = false;
-            }
-            entity.setCommentaireTrie(lTrie);
-//            System.out.println(entity.getCommentaires());
-//            System.out.println(entity.getCommentaireTrie());
-        }
-
-        for (Entry entry : mapCategorie.entrySet()) {
-            System.out.println(entry);
-        }
     }
 }
--- Datamining/src/epsi/i5/datamining/Traitement.java
+++ Datamining/src/epsi/i5/datamining/Traitement.java
@@ -5,10 +5,89 @@
  */
 package epsi.i5.datamining;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Sinys
  */
 public class Traitement {
-    
+
+    StopWords stopWords = new StopWords();
+    private List<DataEntity> lCommentaires;
+    private HashMap<String, HashMap<String, Integer>> mapCategorie = new HashMap();
+    private List<String> words = new ArrayList<>();
+
+    public void traitement() {
+        JsonBuilder builder = new JsonBuilder();
+        StopWords stopword = new StopWords();
+        boolean bStopWord = false;
+        lCommentaires = builder.listeCommentaires;
+        for (DataEntity entity : builder.listeCommentaires) {
+            String lTrie = "";
+
+            for (String cat : entity.getCategorie()) {
+                if (!mapCategorie.containsKey(cat)) {
+                    mapCategorie.put(cat, new HashMap<String, Integer>());
+                }
+            }
+
+            for (String word : entity.getCommentaires().split(" ")) {
+                //System.out.println(stopword.getRegEx());
+                for (String stopApo : stopword.getRegExApos().replace("|", " ").split(" ")) {
+                    word = word.replaceAll(stopApo, "");
+//                    System.out.println(stopApo);
+                }
+
+                word = word.replace(".", " ").replace(",", " ").replace("!", " ").replace("(", "").replace(")", "").replace("'", "").replace(":", "").trim();
+
+                for (String stop : stopword.getRegEx().replace("|", " ").split(" ")) {
+                    //System.out.println(stop);
+                    if ((word.equalsIgnoreCase(stop)) && !word.equalsIgnoreCase("")) {
+                        bStopWord = true;
+                    }
+                }
+                if (bStopWord == false && !"".equals(word)) {
+                    lTrie = lTrie + " " + word;
+
+                    //Remplissage de la map
+                    for (String cat : entity.getCategorie()) {
+                        if (mapCategorie.get(cat).containsKey(word)) {
+                            Integer occurs = mapCategorie.get(cat).get(word);
+                            occurs++;
+                            mapCategorie.get(cat).remove(word);
+                            mapCategorie.get(cat).put(word, occurs);
+                        } else {
+                            mapCategorie.get(cat).put(word, 1);
+                        }
+                    }
+
+                    if (!words.contains(word)) {
+                        words.add(word);
+                    }
+
+                }
+                bStopWord = false;
+            }
+            entity.setCommentaireTrie(lTrie);
+//            System.out.println(entity.getCommentaires());
+//            System.out.println(entity.getCommentaireTrie());
+        }
+
+        for (Map.Entry entry : mapCategorie.entrySet()) {
+            System.out.println(entry);
+        }
+
+        for (String word : words) {
+            Integer max = 0;
+            HashMap catMax;
+            for (Map.Entry entry : mapCategorie.entrySet()) {
+//                if ()
+            }
+        }
+    }
+
 }
